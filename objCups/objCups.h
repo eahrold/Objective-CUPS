7d6b0e620b72ae75d47bdb72403a8951145b5fc7
Eldon Ahrold <eldon.ahrold@gmail.com>
2014-01-21 14:55:28 -0600

    Initial Commit

---
+++ objCups/objCups.h
@@ -0,0 +1,13 @@
+//
+//  objCups.h
+//  objCups
+//
+//  Created by Eldon on 1/21/14.
+//  Copyright (c) 2014 Loyola University New Orleans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objCups : NSObject
+
+@end
---
+++ objCups/objCups.m
@@ -0,0 +1,13 @@
+//
+//  objCups.m
+//  objCups
+//
+//  Created by Eldon on 1/21/14.
+//  Copyright (c) 2014 Loyola University New Orleans. All rights reserved.
+//
+
+#import "objCups.h"
+
+@implementation objCups
+
+@end
