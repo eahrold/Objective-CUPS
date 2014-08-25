9de0e82b56c3aa2f9e1cc4fe92d93b9523a24f16
Marian Zange <marian.zange@ezeep.com>
2014-08-25 12:51:30 +0200

    Make CUPSManager code style more consistent

--- Objective-CUPS/CUPSManager.h
+++ Objective-CUPS/CUPSManager.h
@@ -4,35 +4,36 @@
 //
 //  Copyright (c) 2014 Eldon Ahrold ( https://github.com/eahrold/Objective-CUPS )
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import <Foundation/Foundation.h>
-@class Printer,PrintJob;
+
+@class Printer, PrintJob;
 
 /**
  *  Interface to the CUPS system 
  */
 @interface CUPSManager : NSObject
 
-+(CUPSManager*)sharedManager;
++ (CUPSManager*)sharedManager;
+
 /**
  Adds a Printer
  @param printer a populated Printer Object
@@ -42,8 +43,8 @@
  @return Returns `YES` if printer was successfully added, or `NO` on failure.
  
  */
--(BOOL)addPrinter:(Printer*)printer;
--(BOOL)addPrinter:(Printer*)printer error:(NSError**)error;
+- (BOOL)addPrinter:(Printer*)printer;
+- (BOOL)addPrinter:(Printer*)printer error:(NSError**)error;
 
 /**
  remove a Printer
@@ -51,10 +52,11 @@
  @param error initialized and set if error occurs
  @return `YES` if printer was successfully remvoed, `NO` on failure
  */
--(BOOL)removePrinter:(NSString*)printer;
--(BOOL)removePrinter:(NSString*)printer error:(NSError**)error;
+- (BOOL)removePrinter:(NSString*)printer;
+- (BOOL)removePrinter:(NSString*)printer error:(NSError**)error;
 
 #pragma mark - Options
+
 /**
  Adds a single option to the specified printer
  @param option single option.
@@ -63,7 +65,8 @@
  @note must conform to lpoptions syntax
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)addOption2:(NSString*)option toPrinter:(NSString*)printer;     // add single option conforming to lpoptions syntax
+- (BOOL)addOption2:(NSString*)option toPrinter:(NSString*)printer;     // add single option conforming to lpoptions syntax
+
 /**
  Adds an array of options to the specified printer
  @param options array of option
@@ -71,7 +74,7 @@
  @note must conform to lpoptions syntax
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)addOptions2:(NSArray *)options toPrinter:(NSString*)printer;   // add list option conforming to lpoptions syntax
+- (BOOL)addOptions2:(NSArray *)options toPrinter:(NSString*)printer;   // add list option conforming to lpoptions syntax
 
 /**
  Adds a single option to the specified printer, parsing to PPD file
@@ -82,7 +85,7 @@
  @note must conform to lpoptions syntax
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)addOption:(NSArray *)opt toPrinter:(NSString *)printer;
+- (BOOL)addOption:(NSArray *)opt toPrinter:(NSString *)printer;
 
 /**
  Adds an array of options to the specified printer. Use with OS X
@@ -93,17 +96,18 @@
  @note must conform to lpoptions syntax
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)addOptions:(NSArray *)options toPrinter:(NSString*)printer;   // add list option conforming to lpoptions syntax
+- (BOOL)addOptions:(NSArray *)options toPrinter:(NSString*)printer;   // add list option conforming to lpoptions syntax
 
 #pragma mark - Status Modifier
+
 /**
  Enable Printer
  @param printer the name of the printer destination as registered with CUPS
  @param error initialized and set if error occurs
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)enablePrinter:(NSString*)printer;
--(BOOL)enablePrinter:(NSString *)printer error:(NSError**)error;
+- (BOOL)enablePrinter:(NSString*)printer;
+- (BOOL)enablePrinter:(NSString *)printer error:(NSError**)error;
 
 /**
  Disable Printer
@@ -111,10 +115,11 @@
  @param error initialized and set if error occurs
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)disablePrinter:(NSString*)printer;
--(BOOL)disablePrinter:(NSString *)printer error:(NSError**)error;
+- (BOOL)disablePrinter:(NSString*)printer;
+- (BOOL)disablePrinter:(NSString *)printer error:(NSError**)error;
 
 #pragma mark - Printer Jobs
+
 /**
  description
  @param File Path to the file to print
@@ -122,8 +127,8 @@
  @param error initialized and set if error occurs
  @return Returns `YES` on success, or `NO` on failure.
  */
--(PrintJob*)sendFile:(NSString*)file toPrinter:(NSString*)printer;
--(PrintJob*)sendFile:(NSString*)file toPrinter:(NSString*)printer error:(NSError**)error;
+- (PrintJob*)sendFile:(NSString*)file toPrinter:(NSString*)printer;
+- (PrintJob*)sendFile:(NSString*)file toPrinter:(NSString*)printer error:(NSError**)error;
 
 /**
  *  Send a file to a printer and monitor using a reply block
@@ -133,10 +138,10 @@
  *  @param error   error initialized and set if error occurs
  *  @param watch   block used to monitor the status of the Print job;
  */
--(void)sendFile:(NSString *)file
-      toPrinter:(NSString *)printer
-        failure:(void (^)(NSError *error))failure
-          watch:(void (^)(NSString *status,NSInteger jobID))watch;
+- (void)sendFile:(NSString *)file
+       toPrinter:(NSString *)printer
+         failure:(void (^)(NSError *error))failure
+           watch:(void (^)(NSString *status,NSInteger jobID))watch;
 
 /**
  description
@@ -146,32 +151,36 @@
  @param error initialized and set if error occurs
  @return Returns `YES` on success, or `NO` on failure.
  */
--(PrintJob*)sendFileAtURL:(NSURL*)file toPrinter:(NSString*)printer;
--(PrintJob*)sendFileAtURL:(NSURL *)file toPrinter:(NSString*)printer error:(NSError**)error;
+- (PrintJob*)sendFileAtURL:(NSURL*)file toPrinter:(NSString*)printer;
+- (PrintJob*)sendFileAtURL:(NSURL *)file toPrinter:(NSString*)printer error:(NSError**)error;
 
 /**
  description
  @param
  @return Returns `YES` on success, or `NO` on failure.
  */
--(BOOL)cancelJobsOnPrinter:(NSString*)printer;
--(BOOL)cancelJobsOnPrinter:(NSString*)printer error:(NSError**)error;
+- (BOOL)cancelJobsOnPrinter:(NSString*)printer;
+- (BOOL)cancelJobsOnPrinter:(NSString*)printer error:(NSError**)error;
+
 #pragma mark - Class Methods
+
 /**
  Gets a list with Populated Printer Objects of the currently installed printers
  @return NSSet of Printers Objects
  */
-+(NSSet *)installedPrinters;
++ (NSSet *)installedPrinters;
+
 /**
  Gets a list of the currently installed printers by name
  @return NSSet with the names of the installed printers
  */
-+(NSSet *)namesOfInstalledPrinters;
++ (NSSet *)namesOfInstalledPrinters;
+
 /**
  Gets a list of the options avaliable for particular printer model
  @return NSSet of installed printers
  */
-+(NSArray *)optionsForModel:(NSString*)model;
++ (NSArray *)optionsForModel:(NSString*)model;
 
 /**
  *  get an array of ppds for the given model
@@ -180,6 +189,6 @@
  *
  *  @return array with avaliable ppds for the particular model
  */
-+(NSArray *)ppdsForModel:(NSString *)model;
++ (NSArray *)ppdsForModel:(NSString *)model;
 
 @end
